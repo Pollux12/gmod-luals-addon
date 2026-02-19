fec979ddb8e141bb4dec3b5aa3d5ab1e6b832807
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-02-19 03:46:51 +0000

    Fix custom class scope for Vectors

--- custom/class.Color.lua
+++ custom/class.Color.lua
@@ -3,4 +3,4 @@
 ---@field g number The green component of the color.
 ---@field b number The blue component of the color.
 ---@field a number The alpha component of the color.
-local Color = {}
+Color = {}
