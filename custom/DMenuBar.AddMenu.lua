7d3d0bebf984328dd71c4d02ec580240c9cf7913
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-02-19 22:36:54 +0000

    Add panel/tool overrides and easier lua regeneration

---
+++ custom/DMenuBar.AddMenu.lua
@@ -0,0 +1,6 @@
+---Creates a new menu panel on the menu bar.
+---
+---[View wiki](https://wiki.facepunch.com/gmod/DMenuBar:AddMenu)
+---@param label string The name (label) of the menu to create.
+---@return DMenu # The created DMenu panel.
+function DMenuBar:AddMenu(label) end
