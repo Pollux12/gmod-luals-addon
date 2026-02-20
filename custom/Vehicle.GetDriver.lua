cc5bdf93301f579aef55e958a0d551acc1380fd6
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-02-20 10:11:25 +0000

    Add player return to Vehicle:GetDriver
    Only players can enter vehicles and every other vehicle related thing is Player.

---
+++ custom/Vehicle.GetDriver.lua
@@ -0,0 +1,4 @@
+---Gets the driver of the vehicle, returns NULL Entity if no driver is present.
+---In Garry's Mod, only Players can drive vehicles.
+---@return Player driver The driver of the vehicle.
+function Vehicle:GetDriver() end
