2d4e4f75d0aaa3de12f355572419926ef5319eeb
Pollux <39353174+Pollux12@users.noreply.github.com>
2026-02-27 12:52:22 +0000

    Fix failing tests

--- __tests__/test-data/offline-sites/gmod-wiki/panel-slider.ts
+++ __tests__/test-data/offline-sites/gmod-wiki/panel-slider.ts
@@ -39,5 +39,5 @@ end
 export const apiDefinition =
   `--- A simple slider featuring an numeric display.
 ---@deprecated Panel:SetActionFunction and Panel:PostMessage.         Use DNumSlider instead.
----@class Slider : Panel
+---@class (partial) Slider : Panel
 local Slider = {}\n\n`;
